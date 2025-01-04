b68a8a6106a349c9d18d14f60d5b8e435caa6e17
MahmoudNasrZ <Mahmoudn.zalabany256@gmail.com>
2025-01-04 14:12:46 +0200

    Structure V1

--- src/main.js
+++ src/main.js
@@ -4,8 +4,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
-  <i class="fa-brands fa-facebook"></i>
-  <i class="fa-brands fa-facebook"></i>
+ 
 `;
 
 setupCounter(document.querySelector("#counter"));
