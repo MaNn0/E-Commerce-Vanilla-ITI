5f98a07acc922c4d5a3c7d0f4c791efb65edcaea
MaNn0 <mlolo80013@gmail.com>
2025-01-04 21:26:59 +0200

    fetch api

--- src/main.js
+++ src/main.js
@@ -12,9 +12,34 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 // `;
 
-// console.log("hi");
+const getData = async function () {
+  try {
+    const response = await fetch("http://localhost:3000/products");
+    const data = await response.json();
+    renderImg(data);
+    return data;
+  } catch (err) {
+    console.error(`data is not found ${err}`);
+  }
+};
+export const data = await getData();
 
-fetch("http://localhost:3000/products")
-  .then((response) => response.json())
-  .then((data) => data);
-// console.log(data.category.audo);
+console.log(data);
+
+function renderImg(data) {
+  const img = document.querySelector(".row");
+  const product = data.map((element) => {
+    return `
+      <div class="card mx-3" style="width: 18rem;">
+        <img src="${element.image}" class="card-img-top productCard" alt="${element.title}">
+        <div class="card-body">
+          <h5 class="card-title">${element.title}</h5>
+          <p class="card-text text-danger">Price: ${element.price}$</p>
+          <p class="card-text text-danger">Discount ${element.discount}%</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>
+        `;
+  });
+  img.innerHTML = product;
+}
