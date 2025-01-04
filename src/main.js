c297b3a7c2956406dc0b8aae757b6e3d41633b0f
MaNn0 <mlolo80013@gmail.com>
2025-01-04 15:19:49 +0200

    adding pages

--- src/main.js
+++ src/main.js
@@ -1,10 +1,69 @@
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import { setupCounter } from "./counter.js";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-document.querySelector("#app").innerHTML = `
- 
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+document.querySelector("#NavBar").innerHTML = `
+        <a class="navbar-brand">Navbar</a>
+        <!-- NavBar Search Here -->
+        <form class="d-flex" role="search">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
 `;
 
-setupCounter(document.querySelector("#counter"));
+document.querySelector("#slide").innerHTML = `
+<div
+        id="carouselExampleAutoplaying"
+        class="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img
+              src="./public/Images/861724.png"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="./public/Images/1331316.png"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="./public/Images/1330031.jpeg"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleAutoplaying"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleAutoplaying"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+`;
