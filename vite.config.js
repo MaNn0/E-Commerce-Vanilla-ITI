e0a615283ad3ac35f227a3b172a529decb8f1a95
MahmoudNasrZ <Mahmoudn.zalabany256@gmail.com>
2025-01-07 23:45:12 +0200

    Product+ProductDetails StructureV1

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,14 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "./index.html"),
         profile: resolve(__dirname, "./src/Pages/Profile/Profile.html"),
+        cart: resolve(__dirname, "./src/Pages/Cart/cart.html"),
+        payment: resolve(__dirname, "./src/Pages/Payment/Payment.html"),
+        register: resolve(__dirname, "./src/Pages/Register/register.html"),
+        product: resolve(__dirname, "./src/Pages/products/product.html"),
+        productdetails: resolve(
+          __dirname,
+          "./src/Pages/products/productdetails/productdetails.html"
+        ),
       },
     },
   },
