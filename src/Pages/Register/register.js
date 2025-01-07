f12d5f1c0a9cfa7e55a06a4e601728614c2d5e09
MaNn0 <mlolo80013@gmail.com>
2025-01-07 16:38:38 +0200

    register

--- src/Pages/Register/register.js
+++ src/Pages/Register/register.js
@@ -1,10 +1,18 @@
-const switchBtn = document.querySelector(".switchBtn");
-switchBtn.addEventListener("click", () => {
-  const swtichbtn = document.querySelector(".switchBtn");
-  if (swtichbtn.innerHTML === "Register") {
-    swtichbtn.innerHTML = "Login";
-    document.querySelector(".email").classList.add = "d-none";
-  } else {
-    swtichbtn.innerHTML = "Register";
-  }
+const switchBtnLogin = document.querySelector(".switchBtnLogin");
+const switchBtnRegister = document.querySelector(".switchBtnRegister");
+const hideLogin = document.querySelector("#toggleHideLogin");
+const hideRegister = document.querySelector("#toggleHideRegister");
+
+switchBtnLogin.addEventListener("click", () => {
+  hideLogin.classList.add("hide");
+  hideRegister.classList.remove("hide");
+  hideRegister.classList.add("show");
+  hideLogin.classList.remove("show");
+});
+
+switchBtnRegister.addEventListener("click", () => {
+  hideLogin.classList.add("show");
+  hideRegister.classList.remove("show");
+  hideRegister.classList.add("hide");
+  hideLogin.classList.remove("hide");
 });
