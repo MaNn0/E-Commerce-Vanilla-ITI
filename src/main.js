0c9373a58cc4069cc986f5639253a97b466d0480
MaNn0 <mlolo80013@gmail.com>
2025-01-09 14:17:07 +0200

    adding clothes jelwery & electronics

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
   try {
     const response = await fetch("http://localhost:3000/products");
     const data = await response.json();
+    renderClothing(data);
+    renderJewelery(data);
+    renderElectronics(data);
     renderImg(data);
     renderGaming(data);
     renderTv(data);
@@ -19,10 +22,83 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 // export const data = await getData();
 
+function renderClothing(data) {
+  const img = document.querySelector(".clothing");
+  const product = data
+    .filter((element) => element.category === "men's clothing")
+    .slice(0, 5)
+    .map((element) => {
+      return `
+    <div class="card mx-2 border border-primary shadow p-3 mb-5 bg-body-tertiary rounded" style="width: 18rem;">
+      <img src="${element.image}" class="card-img-top productCard" alt="${
+        element.title
+      }">
+      <div class="card-body d-flex flex-column ">
+        <h5 class="card-title">${element.title}</h5>
+        <p class="card-text text-danger mt-auto">Price: ${element.price}$</p>
+        <p class="card-text text-success">Discount ${element.discount || 0}%</p>
+        <a href="#" class="btn mt-auto btn-primary">Go somewhere</a>
+      </div>
+      </div>
+      `;
+    })
+    .join("");
+  img.innerHTML = product;
+}
+
+function renderJewelery(data) {
+  const img = document.querySelector(".jewelery");
+  const product = data
+    .filter((element) => element.category === "jewelery")
+    .slice(0, 5)
+    .map((element) => {
+      return `
+    <div class="card mx-2 border border-primary shadow p-3 mb-5 bg-body-tertiary rounded" style="width: 18rem;">
+      <img src="${element.image}" class="card-img-top productCard" alt="${
+        element.title
+      }">
+      <div class="card-body d-flex flex-column ">
+        <h5 class="card-title">${element.title}</h5>
+        <p class="card-text text-danger mt-auto">Price: ${element.price}$</p>
+        <p class="card-text text-success">Discount ${element.discount || 0}%</p>
+        <a href="#" class="btn mt-auto btn-primary">Go somewhere</a>
+      </div>
+      </div>
+      `;
+    })
+    .join("");
+  img.innerHTML = product;
+}
+
+function renderElectronics(data) {
+  const img = document.querySelector(".electronics");
+  const product = data
+    .filter((element) => element.category === "electronics")
+    .slice(0, 5)
+    .map((element) => {
+      return `
+    <div class="card mx-2 border border-primary shadow p-3 mb-5 bg-body-tertiary rounded" style="width: 18rem;">
+      <img src="${element.image}" class="card-img-top productCard" alt="${
+        element.title
+      }">
+      <div class="card-body d-flex flex-column ">
+        <h5 class="card-title">${element.title}</h5>
+        <p class="card-text text-danger mt-auto">Price: ${element.price}$</p>
+        <p class="card-text text-success">Discount ${element.discount || 0}%</p>
+        <a href="#" class="btn mt-auto btn-primary">Go somewhere</a>
+      </div>
+      </div>
+      `;
+    })
+    .join("");
+  img.innerHTML = product;
+}
+
 function renderMobile(data) {
   const img = document.querySelector(".mobile");
   const product = data
     .filter((element) => element.category === "mobile")
+    .slice(0, 5)
     .map((element) => {
       return `
     <div class="card mx-2 border border-primary shadow p-3 mb-5 bg-body-tertiary rounded" style="width: 18rem;">
@@ -45,6 +121,7 @@ function renderImg(data) {
   const img = document.querySelector(".audio");
   const product = data
     .filter((element) => element.category === "audio")
+    .slice(0, 5)
     .map((element) => {
       return `
     <div class="card mx-2 border border-primary shadow p-3 mb-5 bg-body-tertiary rounded" style="width: 18rem;">
@@ -68,6 +145,7 @@ function renderGaming(data) {
   const img = document.querySelector(".gaming");
   const product = data
     .filter((element) => element.category === "gaming")
+    .slice(0, 5)
     .map((element) => {
       return `
       <div class="card mx-3 border border-primary shadow p-3 mb-5 bg-body-tertiary rounded" style="width: 18rem;">
@@ -93,6 +171,7 @@ function renderTv(data) {
   const img = document.querySelector(".tv");
   const product = data
     .filter((element) => element.category === "tv")
+    .slice(0, 5)
     .map((element) => {
       return `
       <div class="card mx-3 border border-primary shadow p-3 mb-5 bg-body-tertiary rounded" style="width: 18rem;">
