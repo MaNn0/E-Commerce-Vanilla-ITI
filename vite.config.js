24a5ccdf87887b46bf9aaa1f67f78fa7709e4a1f
MaNn0 <mlolo80013@gmail.com>
2025-02-24 22:32:03 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,25 +2,26 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "./",
+  root: ".",
+
   build: {
-    target: "esnext", // Correct location for the target option
+    outDir: "dist",
+    target: "esnext",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "/"),
-        profile: resolve(__dirname, "/src/Pages/Profile/Profile.html"),
-        cart: resolve(__dirname, "/src/Pages/Cart/cart.html"),
-        payment: resolve(__dirname, "/src/Pages/Payment/Payment.html"),
-        register: resolve(__dirname, "/src/Pages/Register/register.html"),
-        product: resolve(__dirname, "/src/Pages/products/products.html"),
+        main: resolve(__dirname, "index.html"), // Fix this
+        profile: resolve(__dirname, "src/Pages/Profile/Profile.html"),
+        cart: resolve(__dirname, "src/Pages/Cart/cart.html"),
+        payment: resolve(__dirname, "src/Pages/Payment/Payment.html"),
+        register: resolve(__dirname, "src/Pages/Register/register.html"),
+        product: resolve(__dirname, "src/Pages/products/products.html"),
         productdetails: resolve(
           __dirname,
-          "/src/Pages/products/productdetails/productdetails.html"
+          "src/Pages/products/productdetails/productdetails.html"
         ),
-        fail: resolve(__dirname, "/src/Pages/Payment/fail.html"),
-        success: resolve(__dirname, "/src/Pages/Payment/success.html"),
-        myOrder: resolve(__dirname, "/src/Pages/Payment/myOrder.html"),
-        outDir: "dist",
+        fail: resolve(__dirname, "src/Pages/Payment/fail.html"),
+        success: resolve(__dirname, "src/Pages/Payment/success.html"),
+        myOrder: resolve(__dirname, "src/Pages/Payment/myOrder.html"),
       },
     },
   },
